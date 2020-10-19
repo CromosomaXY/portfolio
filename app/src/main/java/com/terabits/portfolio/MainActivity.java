cbd035fc25e19e75317991c6955e2f69ab000d90
CromosomaXY <pisha666@gmail.com>
2020-10-19 12:28:45 +0200

    Definimos los colores de la App

--- app/src/main/java/com/terabits/portfolio/MainActivity.java
+++ app/src/main/java/com/terabits/portfolio/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.terabits.portfolio.cv.CVFragment;
+import com.terabits.portfolio.home.HomeFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        setCVFragment();
+
+        //setHomeFragment();
+    }
+    void setCVFragment(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new CVFragment()).commit();
+    }
+
+
+    void setHomeFragment(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
     }
 }
